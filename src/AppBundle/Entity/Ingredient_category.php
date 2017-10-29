1eae426b1cb8ff86116c11fa430a6d8b692034c1
davidAChiva <david.alfaro.chiva@gmail.com>
2017-10-29 16:07:37 +0100

    Création entity Ingredient_category

---
+++ src/AppBundle/Entity/Ingredient_category.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Ingredient_category
+ *
+ * @ORM\Table(name="ingredient_category")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\Ingredient_categoryRepository")
+ */
+class Ingredient_category
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Ingredient_category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
