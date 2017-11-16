e0a4748a33e3e96f956208fb13d98b371547c39a
davidAChiva <david.alfaro.chiva@gmail.com>
2017-11-16 15:07:22 +0100

    Ajout fonctionnalité ajouter un ingrédient

--- src/AppBundle/Controller/ManageIngredientController.php
+++ src/AppBundle/Controller/ManageIngredientController.php
@@ -4,6 +4,8 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Ingredient;
+use AppBundle\Form\IngredientType;
 
 class ManageIngredientController extends Controller
 
@@ -20,4 +22,25 @@ class ManageIngredientController extends Controller
         ));
     }
 
+    public function addAction(Request $request)
+    {
+        $ingredient = new Ingredient();
+
+        $form = $this->get('form.factory')->create(IngredientType::class,$ingredient);
+        $form->handleRequest($request);
+
+        if ($form->isValid() && $form->isSubmitted())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($ingredient);
+            $em->flush();
+
+            return $this->redirectToRoute('manage_ingredient_index');
+        }
+
+        return $this->render('ManageIngredient/add.html.twig', array(
+            'ingredient'            => $ingredient,
+            'form'                  => $form->createView()
+        ));
+    }
 }
