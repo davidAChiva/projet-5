20c8c74929a4341c4a47db8ac2d0b8fd4b672d44
davidAChiva <david.alfaro.chiva@gmail.com>
2017-11-16 12:14:59 +0100

    Ajout fonctionnalité : ajouter une nouvelle catégorie ingredients

--- src/AppBundle/Controller/ManageIngredientCategoryController.php
+++ src/AppBundle/Controller/ManageIngredientCategoryController.php
@@ -4,6 +4,8 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\IngredientCategory;
+use AppBundle\Form\IngredientCategoryType;
 
 class ManageIngredientCategoryController extends Controller
 
@@ -17,6 +19,28 @@ class ManageIngredientCategoryController extends Controller
 
         return $this->render('ManageIngredientCategory/index.html.twig', array(
             'ingredientCategories'       => $ingredientCategories
+        ));
+    }
+
+    public function addAction(Request $request)
+    {
+        $ingredientCategory = new IngredientCategory();
+
+        $form = $this->get('form.factory')->create(IngredientCategoryType::class,$ingredientCategory);
+        $form->handleRequest($request);
+
+        if ($form->isValid() && $form->isSubmitted())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($ingredientCategory);
+            $em->flush();
+
+            return $this->redirectToRoute('manage_ingredientCategory_index');
+        }
+
+        return $this->render('ManageIngredientCategory/add.html.twig', array(
+            'ingredientCategory'    => $ingredientCategory,
+            'form'                  => $form->createView()
         ));
     }
 
