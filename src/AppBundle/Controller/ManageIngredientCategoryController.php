323b82f60f3a0684b24f293a9486e882ed03f7b1
davidAChiva <david.alfaro.chiva@gmail.com>
2017-11-17 10:43:03 +0100

    Ajout fonctionnalité : Modifier une catégorie ingrédient

--- src/AppBundle/Controller/ManageIngredientCategoryController.php
+++ src/AppBundle/Controller/ManageIngredientCategoryController.php
@@ -2,8 +2,10 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\IngredientType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use AppBundle\Entity\IngredientCategory;
 use AppBundle\Form\IngredientCategoryType;
 
@@ -41,6 +43,36 @@ class ManageIngredientCategoryController extends Controller
         return $this->render('ManageIngredientCategory/add.html.twig', array(
             'ingredientCategory'    => $ingredientCategory,
             'form'                  => $form->createView()
+        ));
+    }
+
+    public function editAction($id, Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $ingredientCategory = $em->getRepository('AppBundle:IngredientCategory')->find($id);
+
+        if (null === $ingredientCategory)
+        {
+            throw new NotFoundHttpException("La catégorie n'existe pas.");
+        }
+
+        $form = $this->get('form.factory')->create(IngredientCategoryType::class,$ingredientCategory);
+        $form->handleRequest($request);
+
+        if ($request->isMethod('POST'))
+        {
+            if ($form->isValid() && $form->isSubmitted())
+            {
+                $em = $this->getDoctrine()->getManager();
+                $em->flush();
+
+                return $this->redirectToRoute('manage_ingredient_category_index');
+            }
+        }
+
+        return $this->render('ManageIngredientCategory/edit.html.twig', array(
+            'ingredientCategory' => $ingredientCategory,
+            'form'   => $form->createView()
         ));
     }
 
