6d281c86fd8c2203db8b4b911436210a5e55e244
davidAChiva <david.alfaro.chiva@gmail.com>
2017-11-17 17:10:54 +0100

    Ajout fonctionnalités : Suppression compte user et admin

--- src/AppBundle/Controller/ManageAccountController.php
+++ src/AppBundle/Controller/ManageAccountController.php
@@ -4,6 +4,8 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use AppBundle\Entity\User;
 
 class ManageAccountController extends Controller
 
@@ -28,6 +30,68 @@ class ManageAccountController extends Controller
 
         return $this->render('ManageAccount/admin.html.twig', array(
             'admins'       => $admins
+        ));
+    }
+
+    public function userDeleteAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('AppBundle:User')->find($id);
+
+        if (null === $user)
+        {
+            throw new NotFoundHttpException('L\'utilisateur n\'existe pas !');
+        }
+
+        // Formulaire qui contient juste le champ crsf pour la sécurité
+        $form = $this->get('form.factory')->create();
+
+        $form->handleRequest($request);
+
+        if ($request->isMethod('POST'))
+        {
+            if ($form->isValid() && $form->isSubmitted())
+            {
+                $em->remove($user);
+                $em->flush();
+
+                return $this->redirectToRoute('manage_account_user');
+            }
+        }
+        return $this->render('ManageAccount/deleteUser.html.twig', array(
+            'user' => $user,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    public function deleteAdminAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $admin = $em->getRepository('AppBundle:User')->find($id);
+
+        if (null === $admin)
+        {
+            throw new NotFoundHttpException('L\'administrateur n\'existe pas !');
+        }
+
+        // Formulaire qui contient juste le champ crsf pour la sécurité
+        $form = $this->get('form.factory')->create();
+
+        $form->handleRequest($request);
+
+        if ($request->isMethod('POST'))
+        {
+            if ($form->isValid() && $form->isSubmitted())
+            {
+                $em->remove($admin);
+                $em->flush();
+
+                return $this->redirectToRoute('manage_account_admin');
+            }
+        }
+        return $this->render('ManageAccount/deleteAdmin.html.twig', array(
+            'admin' => $admin,
+            'form'   => $form->createView(),
         ));
     }
 
