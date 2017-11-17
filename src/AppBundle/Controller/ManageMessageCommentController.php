e6f3a44ce84f637d1218d1b6398dee0ca0d776cd
davidAChiva <david.alfaro.chiva@gmail.com>
2017-11-17 17:53:44 +0100

    Ajout fonctionnalité : Suppression des messages

--- src/AppBundle/Controller/ManageMessageCommentController.php
+++ src/AppBundle/Controller/ManageMessageCommentController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\Request;
 
 class ManageMessageCommentController extends Controller
@@ -17,6 +18,37 @@ class ManageMessageCommentController extends Controller
 
         return $this->render('ManageMessageComment/listMessage.html.twig', array(
             'messages'       => $messages
+        ));
+    }
+
+    public function listMessageDeleteAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $message = $em->getRepository('AppBundle:Contact')->find($id);
+
+        if (null === $message)
+        {
+            throw new NotFoundHttpException('L\'utilisateur n\'existe pas !');
+        }
+
+        // Formulaire qui contient juste le champ crsf pour la sécurité
+        $form = $this->get('form.factory')->create();
+
+        $form->handleRequest($request);
+
+        if ($request->isMethod('POST'))
+        {
+            if ($form->isValid() && $form->isSubmitted())
+            {
+                $em->remove($message);
+                $em->flush();
+
+                return $this->redirectToRoute('manage_message_comment_list_message');
+            }
+        }
+        return $this->render('ManageMessageComment/deleteListMessage.html.twig', array(
+            'message' => $message,
+            'form'   => $form->createView(),
         ));
     }
 
