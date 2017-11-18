2b7dae0fcf6ca6eebd2b9d76ece6f92a19528062
davidAChiva <david.alfaro.chiva@gmail.com>
2017-11-18 10:28:49 +0100

    Ajout index et delete ManageCommentRecipe

--- src/AppBundle/Controller/ManageCommentRecipeController.php
+++ src/AppBundle/Controller/ManageCommentRecipeController.php
@@ -21,4 +21,35 @@ class ManageCommentRecipeController extends Controller
         ));
     }
 
+    public function deleteAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $comment = $em->getRepository('AppBundle:CommentRecipe')->find($id);
+
+        if (null === $comment)
+        {
+            throw new NotFoundHttpException('Le commentaire n\'existe pas !');
+        }
+
+        // Formulaire qui contient juste le champ crsf pour la sécurité
+        $form = $this->get('form.factory')->create();
+
+        $form->handleRequest($request);
+
+        if ($request->isMethod('POST'))
+        {
+            if ($form->isValid() && $form->isSubmitted())
+            {
+                $em->remove($comment);
+                $em->flush();
+
+                return $this->redirectToRoute('manage_comment_recipe_index');
+            }
+        }
+        return $this->render('ManageCommentRecipe/delete.html.twig', array(
+            'comment' => $comment,
+            'form'   => $form->createView(),
+        ));
+    }
+
 }
