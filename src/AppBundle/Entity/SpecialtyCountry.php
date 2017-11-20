9f47dd06790ed6df169a5cd017d0b25c005c6c4f
davidAChiva <david.alfaro.chiva@gmail.com>
2017-11-20 14:29:30 +0100

    Gestion sécurité SpecialtyCountry

--- src/AppBundle/Entity/SpecialtyCountry.php
+++ src/AppBundle/Entity/SpecialtyCountry.php
@@ -3,12 +3,15 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * SpecialtyCountry
  *
  * @ORM\Table(name="specialty_country")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\SpecialtyCountryRepository")
+ * @UniqueEntity(fields="country", message="Le pays existe déjà")
  */
 class SpecialtyCountry
 {
@@ -25,11 +28,18 @@ class SpecialtyCountry
      * @var string
      *
      * @ORM\Column(name="country", type="string", length=255, unique=true)
+     * @Assert\Length(
+     *      min = 3,
+     *      max = 35,
+     *      minMessage = "Le nom du pays doit contenir au moins {{ limit }} caractéres",
+     *      maxMessage = "Le nom du pays ne doit pas dépasser {{ limit }} caractéres"
+     * )
      */
     private $country;
 
     /**
      * @ORM\OneToOne(targetEntity="AppBundle\Entity\Image", cascade={"persist", "remove"})
+     * @Assert\Valid()
      */
     private $image;
     
