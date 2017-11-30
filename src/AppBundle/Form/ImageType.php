1abf183bd6db4ecda12922eda0fc7f37398e1546
davidAChiva <david.alfaro.chiva@gmail.com>
2017-11-30 16:46:38 +0100

    Adaptation des formulaires pour rendre le champ image non obligatoire  quand on édite une entité

--- src/AppBundle/Form/ImageType.php
+++ src/AppBundle/Form/ImageType.php
@@ -6,6 +6,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 class ImageType extends AbstractType
 {
@@ -14,10 +16,36 @@ class ImageType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('file', FileType::class);
+        // Evenement pour rendre obligatoire l'ajout d'image si on édite une entité
+        $builder->addEventListener(
+
+            FormEvents::PRE_SET_DATA,
+
+            function(FormEvent $event)
+            {
+                $image = $event->getData();
+
+                if (null === $image)
+                {
+                    return;
+                }
+
+                // Si il y a pas encore d'image alors on ajoute le formulaire obligatoire
+                if ( null === $image->getId())
+                {
+                    // Alors on ajoute le champ published
+                    $event->getForm()->add('file', FileType::class);
+                }
+                // Sinon, on rend le champ pas obligatoire
+                else
+                {
+                    // Sinon, on le supprime
+                    $event->getForm()->add('file', FileType::class, array('required'   => false));
+                }
+            }
+        );
     }
-    
+
     /**
      * {@inheritdoc}
      */
